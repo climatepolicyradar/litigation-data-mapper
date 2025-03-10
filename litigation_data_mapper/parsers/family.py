a5e6aabcfc7f90d1be9eb71b0a6bd25aed1afc84
Osneil Drakes <osneildrakes@MacBookPro.communityfibre.co.uk>
2025-03-10 11:28:32 +0000

    feat: create function that maps us and global cases

--- litigation_data_mapper/parsers/family.py
+++ litigation_data_mapper/parsers/family.py
@@ -2,8 +2,44 @@ from typing import Any, Optional
 
 import click
 
+from litigation_data_mapper.parsers.helpers import map_global_jurisdictions
 
-def map_event(debug: bool) -> list[Optional[dict[str, Any]]]:
+
+def process_global_case_data(family_data, geographies, case_id):
+    pass
+
+
+def process_us_case_metadata(family_data, case_id):
+    pass
+
+
+def process_us_case_data(family_data, case_id):
+    pass
+
+
+def get_jurisdiction_iso_codes(
+    family: dict[str, Any], mapped_jurisdictions: dict[str, dict[str, str]]
+) -> list[str]:
+    """Retrieve the ISO codes for jurisdictions specified in the family data.
+
+    This function checks the jurisdiction IDs in the provided family data against
+    a mapping of jurisdictions to their ISO codes. It returns a list of ISO codes
+    for the valid jurisdiction IDs. If no valid jurisdiction IDs are found, it
+    returns a default value.
+
+    :param family: A dictionary containing family data, which includes jurisdiction IDs.
+    :param mapped_jurisdictions: A dictionary mapping jurisdiction IDs to their ISO codes.
+    :return: A list of ISO codes for the jurisdictions, or a default value if none are found.
+    """
+
+    # International : XAA
+
+    return []
+
+
+def map_families(
+    families_data: dict[str, list[dict[str, Any]]], debug: bool
+) -> list[Optional[dict[str, Any]]]:
     """Maps the litigation case information to the internal data structure.
 
     This function transforms family data, which the Sabin Centre refers to as
@@ -19,4 +55,51 @@ def map_event(debug: bool) -> list[Optional[dict[str, Any]]]:
     if debug:
         click.echo("📝 No Litigation family data to wrangle.")
 
-    return []
+    global_cases = families_data.get("global_cases", [])
+    us_cases = families_data.get("us_cases", [])
+    jurisdictions = families_data.get("jurisdictions", [])
+
+    mapped_jurisdictions = map_global_jurisdictions(jurisdictions)
+
+    if not global_cases or not us_cases:
+        missing_dataset = "global" if not global_cases else "US"
+        click.echo(
+            f"🛑 No {missing_dataset} cases found in the data. Skipping family litigation."
+        )
+        return []
+
+    if not jurisdictions:
+        click.echo(
+            "🛑 No global or US cases found in the data. Skipping family litigation."
+        )
+        return []
+
+    mapped_families = []
+
+    for index, data in enumerate(us_cases):
+        case_id = data.get("id")
+        if not case_id:
+            click.echo(
+                f"🛑 Skipping US case at index: {index} as it does not contain a case id"
+            )
+            continue
+
+        result = process_us_case_data(data, case_id)
+
+        if result:
+            mapped_families.append(result)
+
+    for index, data in enumerate(global_cases):
+        case_id = data.get("id")
+        if not case_id:
+            click.echo(
+                f"🛑 Skipping global case at index: {index} as it does not contain a case id"
+            )
+            continue
+        geographies = get_jurisdiction_iso_codes(data, mapped_jurisdictions)
+        result = process_global_case_data(data, geographies, case_id)
+
+        if result:
+            mapped_families.append(result)
+
+    return mapped_families
