63c415f1497c24dc89956019cacf6bf97690ef8b
Osneil Drakes <osneildrakes@MacBookPro.communityfibre.co.uk>
2025-03-26 09:14:57 +0000

    refacotr: update event mapper
     - set family document import id value to None, where its not being mapped to a
       document
     - for documents without document ids, also map these to None - this can
       be in instances where its just a motion to file and there is no
       associated physical doc
     - don't map events where the document id is in the skipped document
       context
     - general syntax refactors

--- litigation_data_mapper/parsers/event.py
+++ litigation_data_mapper/parsers/event.py
@@ -27,14 +27,14 @@ def default_event(
     return {
         "import_id": f"Litigation.event.{case_id}.n{0:04}",
         "family_import_id": family_import_id,
-        "family_document_import_id": "",
-        "title": "Filing Year for Action",
-        "event_type_value": "Filing Year for Action",
+        "family_document_import_id": None,
+        "event_title": "Filing Year For Action",
+        "event_type_value": "Filing Year For Action",
         "date": filing_year,
         "metadata": {
-            "event_type": ["Filing Year for Action"],
-            "description": ["Filing Year for Action"],
-            "datetime_event_name": ["Filing Year for Action"],
+            "event_type": ["Filing Year For Action"],
+            "description": ["Filing Year For Action"],
+            "datetime_event_name": ["Filing Year For Action"],
         },
     }
 
@@ -50,11 +50,66 @@ def get_event_type(doc_type: str) -> str | None:
     return event_type.value if event_type else None
 
 
+def map_event(doc, case_type, context, event_import_id, family_import_id, case_id):
+    litigation_doc_type = doc[
+        get_key(
+            case_type,
+            "ccl_document_type",
+            "ccl_nonus_document_type",
+        )
+    ]
+    event_type = get_event_type(litigation_doc_type)
+    if event_type is None:
+        click.echo(
+            f"🛑 Skipping event for case ({case_id}): {litigation_doc_type} is not a valid event type!"
+        )
+        return None
+
+    document_id_key = "ccl_file" if case_type == "case" else "ccl_nonus_file"
+    document_id = doc.get(document_id_key)
+
+    if document_id in context["skipped_documents"]:
+        click.echo(f"🛑 Skipping event: document {document_id} is in skipped context")
+        return None
+
+    document_import_id = None
+    # Not all documents will have file ids, or related document files, for instances such as
+    # motion to file proceedings.
+    if isinstance(document_id, int):
+        document_import_id = f"Litigation.document.{case_id}.{document_id}"
+
+    event_data = {
+        "import_id": event_import_id,
+        "family_import_id": family_import_id,
+        "family_document_import_id": document_import_id,
+        "event_type_value": event_type,
+        "event_title": doc[
+            get_key(case_type, "ccl_document_type", "ccl_nonus_document_type")
+        ],
+        "date": doc[get_key(case_type, "ccl_filing_date", "ccl_nonus_filing_date")],
+        "metadata": {
+            "event_type": [event_type],
+            "description": [
+                doc[
+                    get_key(
+                        case_type,
+                        "ccl_document_summary",
+                        "ccl_nonus_document_summary",
+                    )
+                ]
+            ],
+            "datetime_event_name": ["Filing Year For Action"],
+        },
+    }
+
+    return event_data
+
+
 def process_family_events(
     family: dict,
     case_id: int,
     event_family_counter: dict[str, int],
-    document_family_counter: dict[str, int],
+    context: dict[str, Any],
 ) -> list[dict[str, Any]]:
     """Processes the family- and document-related case events and maps them to the internal data structure.
 
@@ -64,7 +119,7 @@ def process_family_events(
     :param dict family: The family case related data, including family details and related documents.
     :param int case_id: The unique identifier for the case, used to link events to the correct case.
     :param dict[str, int] event_family_counter: A dictionary that tracks the count of events types for each family case.
-    :param dict[str, int] document_family_counter: A dictionary that tracks the count of document types for each family case.
+    :param  dict[str, Any] context: The context of the litigation project import.
     :return list[dict[str, Any]]: A list of mapped family case events in the 'destination' format described in the Litigation Data Mapper Google Sheet, or empty list if no events are found.
     """
     case_type = family.get("type")
@@ -72,7 +127,6 @@ def process_family_events(
     family_events = []
     family_import_id = f"Litigation.family.{case_id}.0"
     initialise_counter(event_family_counter, family_import_id)
-    initialise_counter(document_family_counter, family_import_id)
 
     documents_key = (
         "ccl_case_documents" if case_type == "case" else "ccl_nonus_case_documents"
@@ -106,51 +160,14 @@ def process_family_events(
     if documents:
         for doc in documents:
             event_import_id = f"Litigation.event.{case_id}.n{event_family_counter[family_import_id]:04}"
-            document_import_id = f"Litigation.document.{case_id}.n{document_family_counter[family_import_id]:04}"
-            litigation_doc_type = doc[
-                get_key(
-                    case_type,
-                    "ccl_document_type",
-                    "ccl_nonus_document_type",
-                )
-            ]
-            event_type = get_event_type(litigation_doc_type)
-            if event_type is None:
-                click.echo(
-                    f"🛑 Skipping mapping events for case: {case_id}, {litigation_doc_type} is not a valid event type!"
-                )
-                continue
-            family_events.append(
-                {
-                    "import_id": event_import_id,
-                    "family_import_id": family_import_id,
-                    "family_document_import_id": document_import_id,
-                    "event_type_value": event_type,
-                    "title": doc[
-                        get_key(
-                            case_type, "ccl_document_type", "ccl_nonus_document_type"
-                        )
-                    ],
-                    "date": doc[
-                        get_key(case_type, "ccl_filing_date", "ccl_nonus_filing_date")
-                    ],
-                    "metadata": {
-                        "event_type": [event_type],
-                        "description": [
-                            doc[
-                                get_key(
-                                    case_type,
-                                    "ccl_document_summary",
-                                    "ccl_nonus_document_summary",
-                                )
-                            ]
-                        ],
-                        "datetime_event_name": [event_type],
-                    },
-                }
+            event_data = map_event(
+                doc, case_type, context, event_import_id, family_import_id, case_id
             )
+            if not event_data:
+                continue
+
+            family_events.append(event_data)
             event_family_counter[family_import_id] += 1
-            document_family_counter[family_import_id] += 1
     return family_events
 
 
@@ -163,6 +180,8 @@ def map_events(
     case events, into our internal data modelling structure. It returns a list of
     mapped events, each represented as a dictionary matching the required schema.
 
+     :parm dict[str, Any] events_data: The case related data, structured as global cases,
+        us cases.
     :param  dict[str, Any] context: The context of the litigation project import.
     :return list[dict[str, Any]]: A list of litigation case events in
         the 'destination' format described in the Litigation Data Mapper Google
@@ -184,7 +203,6 @@ def map_events(
     families = us_cases + global_cases
 
     event_family_counter = {}
-    document_family_counter = {}
     mapped_events = []
 
     for index, family in enumerate(families):
@@ -199,9 +217,7 @@ def map_events(
             )
             continue
 
-        result = process_family_events(
-            family, case_id, event_family_counter, document_family_counter
-        )
+        result = process_family_events(family, case_id, event_family_counter, context)
         mapped_events.extend(result)
 
     return mapped_events
