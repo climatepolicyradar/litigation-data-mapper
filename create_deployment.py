273b513ac301bad4c2b06f0e219151bb1683df5d
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-09-24 15:51:49 +0100

    feat: move sync_wordpress_to_s3 to its own flow

--- create_deployment.py
+++ create_deployment.py
@@ -4,7 +4,7 @@ from prefect import Flow
 from prefect.blocks.system import JSON
 from prefect.docker.docker_image import DockerImage
 
-from litigation_data_mapper.flows import automatic_updates
+from litigation_data_mapper.flows import automatic_updates, sync_wordpress_to_s3_flow
 
 MEGABYTES_PER_GIGABYTE = 1024
 DEFAULT_FLOW_VARIABLES = {
@@ -13,9 +13,7 @@ DEFAULT_FLOW_VARIABLES = {
 }
 
 
-def create_deployment(
-    flow: Flow,
-) -> None:
+def create_deployment(flow: Flow, cron: str) -> None:
     """Create a deployment for the specified flow"""
     aws_env = os.environ["AWS_ENV"]
     docker_registry = os.environ["DOCKER_REGISTRY"]
@@ -33,7 +31,7 @@ def create_deployment(
             dockerfile="Dockerfile",
         ),
         # this is scheduled to run daily at midnight
-        cron="0 0 * * *",
+        cron=cron,
         work_queue_name=f"mvp-{aws_env}",
         job_variables=job_variables,
         build=False,
@@ -42,4 +40,8 @@ def create_deployment(
 
 
 if __name__ == "__main__":
-    create_deployment(automatic_updates)
+    # at midnight
+    create_deployment(automatic_updates, cron="0 0 * * *")
+    # every 4 hours from 3-23
+    # this means this will have run before the midnight run above
+    create_deployment(sync_wordpress_to_s3_flow, cron="0 3,7,11,15,19,23 * * *")
